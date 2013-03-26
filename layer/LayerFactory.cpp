d4c878163b4ab92cc05060d61ccf73996d36f576
Matthias Mauch <matthiasmauch@gmail.com>
2013-03-26 20:14:08 +0000

    put the FlexiNote layer before the note layer to see what happens -- not much

--- layer/LayerFactory.cpp
+++ layer/LayerFactory.cpp
@@ -161,11 +161,13 @@ LayerFactory::getValidLayerTypes(Model *model)
     }
 
     if (dynamic_cast<NoteModel *>(model)) {
+	types.insert(FlexiNotes);
 	types.insert(Notes);
     }
 
     if (dynamic_cast<FlexiNoteModel *>(model)) {
 	types.insert(FlexiNotes);
+	types.insert(Notes);
     }
 
     if (dynamic_cast<RegionModel *>(model)) {
@@ -196,8 +198,8 @@ LayerFactory::getValidEmptyLayerTypes()
     LayerTypeSet types;
     types.insert(TimeInstants);
     types.insert(TimeValues);
+	types.insert(FlexiNotes);
     types.insert(Notes);
-    types.insert(FlexiNotes);
     types.insert(Regions);
     types.insert(Text);
     types.insert(Image);
@@ -213,8 +215,8 @@ LayerFactory::getLayerType(const Layer *layer)
     if (dynamic_cast<const TimeRulerLayer *>(layer)) return TimeRuler;
     if (dynamic_cast<const TimeInstantLayer *>(layer)) return TimeInstants;
     if (dynamic_cast<const TimeValueLayer *>(layer)) return TimeValues;
+	if (dynamic_cast<const FlexiNoteLayer *>(layer)) return FlexiNotes;
     if (dynamic_cast<const NoteLayer *>(layer)) return Notes;
-    if (dynamic_cast<const FlexiNoteLayer *>(layer)) return FlexiNotes;
     if (dynamic_cast<const RegionLayer *>(layer)) return Regions;
     if (dynamic_cast<const TextLayer *>(layer)) return Text;
     if (dynamic_cast<const ImageLayer *>(layer)) return Image;
@@ -257,7 +259,7 @@ LayerFactory::getLayerTypeName(LayerType type)
     case TimeInstants: return "timeinstants";
     case TimeValues: return "timevalues";
     case Notes: return "notes";
-    case FlexiNotes: return "flexible notes";
+    case FlexiNotes: return "flexinotes";
     case Regions: return "regions";
     case Text: return "text";
     case Image: return "image";
@@ -278,7 +280,7 @@ LayerFactory::getLayerTypeForName(QString name)
     if (name == "timeruler") return TimeRuler;
     if (name == "timeinstants") return TimeInstants;
     if (name == "timevalues") return TimeValues;
-    if (name == "flexible notes") return FlexiNotes;
+    if (name == "flexinotes") return FlexiNotes;
     if (name == "regions") return Regions;
     if (name == "text") return Text;
     if (name == "image") return Image;
@@ -347,9 +349,9 @@ LayerFactory::createEmptyModel(LayerType layerType, Model *baseModel)
 	return new SparseOneDimensionalModel(baseModel->getSampleRate(), 1);
     } else if (layerType == TimeValues) {
 	return new SparseTimeValueModel(baseModel->getSampleRate(), 1, true);
+	} else if (layerType == FlexiNotes) {
+	return new FlexiNoteModel(baseModel->getSampleRate(), 1, true);
     } else if (layerType == Notes) {
-	return new NoteModel(baseModel->getSampleRate(), 1, true);
-    } else if (layerType == FlexiNotes) {
 	return new NoteModel(baseModel->getSampleRate(), 1, true);
     } else if (layerType == Regions) {
 	return new RegionModel(baseModel->getSampleRate(), 1, true);
@@ -418,12 +420,12 @@ LayerFactory::createLayer(LayerType type)
 	layer = new TimeValueLayer;
 	break;
 
-    case Notes:
-	layer = new NoteLayer;
-	break;
-
     case FlexiNotes:
 	layer = new FlexiNoteLayer;
+	break;
+
+    case Notes:
+	layer = new NoteLayer;
 	break;
 
     case Regions:
