d1c85b9607d10ce9049a4139dfb59c66fac62587
Chris Cannam <c.cannam@qmul.ac.uk>
2014-09-03 11:49:31 +0100

    Clarify some sneaky logic

--- layer/TimeValueLayer.cpp
+++ layer/TimeValueLayer.cpp
@@ -733,13 +733,21 @@ TimeValueLayer::snapToSimilarFeature(View *v, int &frame,
     i = close.begin();
 
     // Scan through the close points first, then the more distant ones
-    // if no suitable close one is found
+    // if no suitable close one is found. So the while-termination
+    // condition here can only happen once i has passed through the
+    // whole of the close container and then the whole of the separate
+    // points container. The two iterators are totally distinct, but
+    // have the same type so we cheekily use the same variable and a
+    // single loop for both.
 
     while (i != points.end()) {
 
-        if (i == close.end()) {
-            i = points.begin();
-            distant = true;
+        if (!distant) {
+            if (i == close.end()) {
+                // switch from the close container to the points container
+                i = points.begin();
+                distant = true;
+            }
         }
 
 	if (snap == SnapRight) {
