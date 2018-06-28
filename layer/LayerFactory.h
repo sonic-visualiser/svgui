2e12b44682ef830afa2d73ef6564ed6982ba9ea7
Chris Cannam <cannam@all-day-breakfast.com>
2018-06-28 14:48:48 +0100

    Rename guard

--- layer/LayerFactory.h
+++ layer/LayerFactory.h
@@ -13,8 +13,8 @@
     COPYING included with this distribution for more information.
 */
 
-#ifndef _LAYER_FACTORY_H_
-#define _LAYER_FACTORY_H_
+#ifndef SV_LAYER_FACTORY_H
+#define SV_LAYER_FACTORY_H
 
 #include <QString>
 #include <set>
